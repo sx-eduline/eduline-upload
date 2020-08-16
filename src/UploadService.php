f9612889e0636e9b28db6ceb95477ca6f676d702
sunyonghong <syh@sunyonghong.com>
2020-08-16 17:05:59 +0800

    优化上传服务类

--- src/UploadService.php
+++ src/UploadService.php
@@ -1,20 +1,19 @@
 <?php
 namespace eduline\upload;
 
-use think\Route;
+use think\facade\Route;
 use think\Service;
 
 class UploadService extends Service
 {
     public function boot()
     {
-        $this->registerRoutes(function (Route $route) {
+        $this->registerRoutes(function () {
             /** 接口路由 */
-            $route->group('admin/system/package/upload', function () use ($route) {
-                $route->get('/list', '@index')->name('system.package.upload'); // 上传配置页面
-                $route->get('/<stock>/config', '@config')->pattern(['stock' => '[a-zA-Z_]+'])->name('system.package.upload.config'); // 上传配置页面
+            Route::group('system/package/upload', function () {
+                Route::get('/list', '@index')->name('system.package.upload'); // 上传配置页面
+                Route::get('/<stock>/config', '@config')->pattern(['stock' => '[a-zA-Z_]+'])->name('system.package.upload.config'); // 上传配置页面
             })->prefix('\eduline\upload\admin\service\Config')->middleware(['adminRoute']);
-
         });
     }
 }
