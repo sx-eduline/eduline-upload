d87f27ca05efe93fbade6f855b4c676913f1e4d9
sunyonghong <syh@sunyonghong.com>
2020-08-16 16:20:30 +0800

    上传类服务注入路由

--- src/UploadService.php
+++ src/UploadService.php
@@ -15,6 +15,6 @@ class UploadService extends Service
                 $route->get('/<stock>/config', '@config')->pattern(['stock' => '[a-zA-Z_]+'])->name('system.package.upload.config'); // 上传配置页面
             })->prefix('\eduline\upload\admin\service\Config')->middleware(['adminRoute']);
 
-        })
+        });
     }
 }
